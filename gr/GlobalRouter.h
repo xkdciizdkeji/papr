1afe3ed740d52195fc7189f621fb5af9179e063c
wxwhsl <71640276+wxwhsl@users.noreply.github.com>
2024-01-30 11:20:26 +0800

    Add files via upload

---
+++ gr/GlobalRouter.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "GridGraph.h"
+#include "GRNet.h"
+#include "../obj/ISPD24Parser.h"
+#include "../multithread/Scheduler.h" 
+#include "../multithread/SingleNetRouter.h"
+
+class GlobalRouter {
+public:
+    GlobalRouter(const ISPD24Parser& parser, const Parameters& params);
+    void route();
+    void write(std::string guide_file = "");
+
+    void update_nonstack_via_counter(unsigned net_idx, const std::vector<std::vector<int>> &via_loc, std::vector<std::vector<std::vector<int>>> &flag, std::vector<std::vector<std::vector<int>>> &nonstack_via_counter) const;
+
+private:
+    const Parameters& parameters;
+    GridGraph gridGraph;
+    std::vector<GRNet> nets;
+    
+    int areaOfPinPatches;
+    int areaOfWirePatches;
+
+    int numofThreads;
+
+    // for evaluation
+    CostT unit_length_wire_cost;
+    CostT unit_via_cost;
+    std::vector<CostT> unit_length_short_costs;
+    
+    void sortNetIndices(std::vector<int>& netIndices) const; 
+    void sortNetIndicesD(std::vector<int> &netIndices) const;
+    void sortNetIndicesOFD(std::vector<int> &netIndices, std::vector<int> &netOverflow) const;
+    void sortNetIndicesOFDALD(std::vector<int> &netIndices, std::vector<int> &netOverflow) const;
+    void sortNetIndicesOLD(std::vector<int> &netIndices) const;
+    void sortNetIndicesOLI(std::vector<int> &netIndices) const;
+    void sortNetIndicesRandom(std::vector<int> &netIndices) const;
+    void getGuides(const GRNet &net, std::vector<std::pair<std::pair<int, int>, utils::BoxT<int>>> &guides);
+    // void getGuides(const GRNet &net, std::vector<std::pair<int, utils::BoxT<int>>> &guides);
+    void printStatistics() const;
+
+    void runJobsMT(int numJobs, int numofThreads, const std::function<void(int)>& handle);
+    void runJobsMTnew(std::vector<std::vector<int>> batches, const std::function<void(int)>& handle);
+    std::vector<std::vector<int>> getBatches(std::vector<SingleNetRouter>& routers, const std::vector<int>& netsToRoute);
+};
---
+++ multithread/Scheduler.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../utils/utils.h"
+#include "SingleNetRouter.h"
+#include <boost/geometry.hpp>
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+using boostPoint = bg::model::point<DBU, 2, bg::cs::cartesian>;
+using boostBox = bg::model::box<boostPoint>;
+using RTree = bgi::rtree<std::pair<boostBox, int>, bgi::linear<16>>; 
+using RTrees = std::vector<bgi::rtree<std::pair<boostBox, int>, bgi::linear<16>>>;
+
+class Scheduler {
+public:
+    Scheduler(const std::vector<SingleNetRouter>& routersToExec,int layer) : routers(routersToExec),layer_num(layer){}
+    // std::vector<std::vector<int>>& scheduleOrderEq(int numofThreads);
+    std::vector<std::vector<int>>& scheduleOrderEq(int numofThread, const std::vector<int>& netsToRoute);
+    int layer_num;
+
+private:
+    const std::vector<SingleNetRouter>& routers;
+    std::vector<std::vector<int>> batches;
+
+    // for conflict detect
+    RTrees rtrees;
+    void initSet(std::vector<int> jobIdxes);
+    void updateSet(int jobIdx);
+    bool hasConflict(int jobIdx);
+};
